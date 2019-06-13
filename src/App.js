95655d7d6f4eff6063ce456844e6e6f21f43d087
Christian Mader <Cmader555@users.noreply.github.com>
2019-06-13 15:13:18 -0700

    added bootstrap, page is being rendered

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+
+
+class App extends Component {
+    // Setting this.state.friends to the friends json array
+    
+    render() {
+      return <h1>Hello World!</h1>
+    }
+  }
+  
+  export default App;
+  
