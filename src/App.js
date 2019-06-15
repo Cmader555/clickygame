c1424a7ae9a3ffeea4c4630234b07101e5d26f95
Christian Mader <Cmader555@users.noreply.github.com>
2019-06-15 13:03:55 -0700

    randomly shuffles/ and tracks scores

--- src/App.js
+++ src/App.js
@@ -7,19 +7,58 @@ import Wrapper from "./components/Wrapper";
 
 class App extends Component {
   state = {
-    characters
+    characters: characters.map(character => ({ ...character, clicked: false })),
+    score: 0,
+    highScore: 0
   }
+
+  handleClick = character => {
+    console.log(character)
+    let { score, highScore, characters } = this.state;
+
+    if (character.clicked === true) {
+      highScore = highScore > score ? highScore : score;
+      this.setState({
+        characters: characters.map(character => ({ ...character, clicked: false })),
+        score: 0,
+        highScore: highScore
+      }, ()=> {this.shuffleArray(this.state.characters)})
+    } else {
+
+      this.setState({
+        score: score++
+      })
+      character.clicked = true;
+
+      this.shuffleArray(characters)
+    }
+  }
+
+  shuffleArray = array => {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    this.setState({
+      characters: array
+    })
+  }
+
 
   render() {
     return (
       <Wrapper>
 
-      {this.state.characters.map(character => (
-        <StarwarsCard 
-          id={character.id}
-          image={character.image}
-        />
-      ))}
+        {this.state.characters.map(character => (
+          <StarwarsCard
+            character={character}
+            onClick={this.handleClick}
+            id={character.id}
+            image={character.image}
+          />
+        ))}
 
 
 
--- src/components/Starwarscard/index.js
+++ src/components/Starwarscard/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function StarwarsCard(props) {
   return (
-    <div className="card">
+    <div className="card" onClick={() => props.onClick(props.character)}>
       <div className="img-container">
         <img  src={props.image} alt={props.name}/>
       </div>
